1cb8f8d874e034d880be5d372cc2417b86512cb9
Mohamed Ibnahmad <ibnahmadmohamed8@gmail.com>
2022-04-22 13:50:45 +0000

    last commit

---
+++ src/components/modals/ContainerModal.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Button from '@/components/base/Button.vue'
+import CloseIcon from '@/components/icons/CloseIcon.vue'
+
+const props = defineProps({
+  value: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+})
+const emits = defineEmits(['close'])
+
+const modalAction = () => {
+  emits('close')
+}
+</script>
+
+
+
+<style>
+
+</style>
---
+++ src/store/modules/vuello-store.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const state = {
+  vuello: null
+}
+
+const getters = {
+  getVuelloDatas(state) {
+    return state.vuello
+  }
+}
+
+const actions = {
+  setVuello({ commit }, data) {
+    commit('SET_VUELLO', data)
+  }
+}
+
+const mutations = {
+  SET_VUELLO(state, data) {
+    state.vuello = data
+    
+  }
+}
+
+const vuelloModule = {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
+
+export default vuelloModule
